c06661f6b6eb6e4d8044a08b6d5abf710bad2dac
Greg <harrison.greg.c@gmail.com>
2015-04-23 15:25:47 -0400

    Updated the UI to integrate the API Responses.

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -7,19 +7,27 @@
             $scope.getUserResponse = function () {
                 User.all()
                     .success(function(data){
-                        toaster.pop('warning', "Link to Toaster Github", 'https://github.com/jirikavi/AngularJS-Toaster', 5000, 'trustedHtml', function(toaster) {
-                            var match = toaster.body.match(/http[s]?:\/\/[^\s]+/);
-                            if (match) $window.open(match[0]);
-                            return true;
-                        });
-                        toaster.success({title: "A Toast!", body: data.message});
                         $scope.name = data.name;
                         $scope.userID = data.userID;
                         $scope.phone = data.phone;
+                        toaster.success({title: "DB Response", body: "Data Returned Successfully"});
                     });
             };
             $scope.postUserInfo = function () {
-
+                toaster.warning({title: "Page Response", body: "Still need to complete POST method"});
+            };
+            $scope.linkToToast = function () {
+                toaster.pop('note', "Link to Toaster Github", 'https://github.com/jirikavi/AngularJS-Toaster', 5000, 'trustedHtml', function(toaster) {
+                    var match = toaster.body.match(/http[s]?:\/\/[^\s]+/);
+                    if (match) $window.open(match[0]);
+                    return true;
+                });
+            };
+            $scope.clearData = function () {
+                $scope.name = "";
+                $scope.userID = "";
+                $scope.phone = "";
+                toaster.error({title: "Page Response", body: "User data has been removed"});
             }
         }
 }());

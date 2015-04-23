f21d43ef8252be8d2098f9e9ec32cdc64ba9899e
Greg <harrison.greg.c@gmail.com>
2015-04-23 14:54:40 -0400

    Added a link to the github/docs for the toaster module

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -3,10 +3,15 @@
         .module('homeController', ['userService', 'ngAnimate', 'toaster'])
         .controller('homeController', homeController);
 
-        function homeController($scope, User, $timeout, toaster) {
+        function homeController($scope, User, $timeout, toaster, $window) {
             $scope.getUserResponse = function () {
                 User.all()
                     .success(function(data){
+                        toaster.pop('warning', "Link to Toaster Github", 'https://github.com/jirikavi/AngularJS-Toaster', 5000, 'trustedHtml', function(toaster) {
+                            var match = toaster.body.match(/http[s]?:\/\/[^\s]+/);
+                            if (match) $window.open(match[0]);
+                            return true;
+                        });
                         toaster.success({title: "A Toast!", body: data.message});
                     });
             };
